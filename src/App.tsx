cb6403facd64465b31ac206850ef943fe3e03f9c
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-10 16:19:52 +0300

    первая версия начальной странички

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,36 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb, Button } from "antd";
+import classNames from "classnames";
+import { Layout, Menu, Breadcrumb, Row, Col, Card } from "antd";
 import {
   UserOutlined,
   LaptopOutlined,
   NotificationOutlined,
+  ArrowUpOutlined,
+  ArrowDownOutlined,
 } from "@ant-design/icons";
-import "./App.css";
+import styles from "./App.module.scss";
 
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <Menu mode="horizontal" defaultSelectedKeys={["2"]}>
+    <Layout className={styles.mainLayout}>
+      <Header>
+        <div className={styles.logo} />
+        <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
           <Menu.Item key="3">nav 3</Menu.Item>
         </Menu>
       </Header>
       <Layout>
-        <Sider width={200} className="site-layout-background">
+        <Sider width={200}>
           <Menu
             mode="inline"
             defaultSelectedKeys={["1"]}
             defaultOpenKeys={["sub1"]}
-            style={{ height: "100%", borderRight: 0 }}
+            className={styles.sideMenu}
           >
             <SubMenu
               key="sub1"
@@ -73,21 +76,102 @@ function App() {
             </SubMenu>
           </Menu>
         </Sider>
-        <Layout style={{ padding: "0 24px 24px" }}>
+        <Layout className={styles.innerLayout}>
           <Breadcrumb style={{ margin: "16px 0" }}>
             <Breadcrumb.Item>Home</Breadcrumb.Item>
             <Breadcrumb.Item>List</Breadcrumb.Item>
             <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
-          <Content
-            className="site-layout-background"
-            style={{
-              padding: 24,
-              margin: 0,
-              minHeight: 280,
-            }}
-          >
-            <Button type="primary">Button</Button>
+          <Content className={styles.content}>
+            <Row gutter={10}>
+              <Col span={6}>
+                <Card>
+                  <h6 className={styles.card__h6}>Conversion rate</h6>
+                  <div className={styles.card__container}>
+                    <h3 className={styles.card__h3}>0.81%</h3>
+                    <p className={styles.card__p}>
+                      <span
+                        className={classNames(
+                          styles.card__span,
+                          styles.card__span_success
+                        )}
+                      >
+                        1.2%
+                        <ArrowUpOutlined />
+                      </span>
+                      <span className={styles.span}> than last week</span>
+                    </p>
+                  </div>
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card>
+                  <h6 className={styles.card__h6}>Conversion rate</h6>
+                  <div className={styles.card__container}>
+                    <h3 className={styles.card__h3}>3,137</h3>
+                    <p className={styles.card__p}>
+                      <span
+                        className={classNames(
+                          styles.card__span,
+                          styles.card__span_danger
+                        )}
+                      >
+                        0.7%
+                        <ArrowDownOutlined />
+                      </span>
+                      <span className={styles.span}> than last week</span>
+                    </p>
+                  </div>
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card>
+                  <h6 className={styles.card__h6}>Conversion rate</h6>
+                  <div className={styles.card__container}>
+                    <h3 className={styles.card__h3}>$306.20</h3>
+                    <p className={styles.card__p}>
+                      <span
+                        className={classNames(
+                          styles.card__span,
+                          styles.card__span_danger
+                        )}
+                      >
+                        0.3%
+                        <ArrowDownOutlined />
+                      </span>
+                      <span className={styles.span}> than last week</span>
+                    </p>
+                  </div>
+                </Card>
+              </Col>
+              <Col span={6}>
+                <Card>
+                  <h6 className={styles.card__h6}>Conversion rate</h6>
+                  <div className={styles.card__container}>
+                    <h3 className={styles.card__h3}>1,650</h3>
+                    <p className={styles.card__p}>
+                      <span
+                        className={classNames(
+                          styles.card__span,
+                          styles.card__span_success
+                        )}
+                      >
+                        2.1%
+                        <ArrowUpOutlined />
+                      </span>
+                      <span className={styles.span}> than last week</span>
+                    </p>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col span={16}></Col>
+              <Col span={8}></Col>
+            </Row>
+            <Row>
+              <Col span={24}></Col>
+            </Row>
           </Content>
         </Layout>
       </Layout>
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./index.css";
+import "./assets/style/app.scss";
+import "./theme.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
