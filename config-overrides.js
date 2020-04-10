5881ee7d296f71598c837778cff816c694f6e953
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-10 16:19:29 +0300

    глобальное переопределение шрифтов

--- config-overrides.js
+++ config-overrides.js
@@ -8,6 +8,12 @@ module.exports = override(
   }),
   addLessLoader({
     javascriptEnabled: true,
-    modifyVars: {},
+    modifyVars: {
+      "@primary-color": "#0168fa",
+      "@black": "#001737",
+      "@font-family": `IBM Plex Sans, -apple-system, BlinkMacSystemFont,
+        'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif,
+        'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
+    },
   })
 );
