e754c7c72a20a70fd10de458129a0f5da0dc0cf1
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-13 23:58:30 +0300

    кастомизация внутреннего элемента компонента

--- src/ui/components/CardInfo/index.tsx
+++ src/ui/components/CardInfo/index.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Card as AntCard } from "antd";
 import styles from "./CardInfo.module.less";
 
-console.log(styles);
-
 type CardProps = {
   children?: React.ReactNode;
 };
 
 const CardInfo = ({ children, ...props }: CardProps) => {
+  console.log(children);
   return (
     <AntCard className={styles.card} {...props}>
       {children}
+      <div className={styles.card_body}></div>
     </AntCard>
   );
 };
