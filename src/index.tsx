c40256a052e7841c39c9a2389131bc9e0f12e042
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-14 10:59:54 +0300

    основной модуль переехал на less

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import "./assets/style/app.scss";
+import "./assets/style/app.less";
 import "./theme.scss";
 
 ReactDOM.render(<App />, document.getElementById("root"));
