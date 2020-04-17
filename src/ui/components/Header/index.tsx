253a53841c66eed1782b67156768b598ccbd75ee
pavke <plaha@yandex.ru>
2020-04-17 15:31:42 +0300

    header: wrap items in link

--- src/ui/components/Header/index.tsx
+++ src/ui/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout, Avatar, Space, Popover, Badge } from "antd";
-import { BellOutlined , SettingOutlined} from "@ant-design/icons";
+import { BellOutlined, SettingOutlined } from "@ant-design/icons";
 import styles from "./Header.module.less";
 
 const { Header: AntHeader } = Layout;
@@ -10,13 +10,19 @@ const Header: React.FC<IHeaderProps> = () => {
   return (
     <AntHeader className={styles.header}>
       <div className={styles.userWrapper}>
-        <Space size="middle">
-          <SettingOutlined />
-          <Popover placement="bottom" title={"title"} content={"content"} trigger="click">
-            <Badge count={4} dot>
-              <BellOutlined />
-            </Badge>
-          </Popover>
+        <Space size="large">
+          <a href="#">
+            <SettingOutlined />
+          </a>
+
+          <a href="#">
+            <Popover placement="bottom" title={"title"} content={"content"} trigger="click">
+              <Badge count={4} dot>
+                <BellOutlined />
+              </Badge>
+            </Popover>
+          </a>
+
           <Space>
             <Avatar size="small" src="http://themepixels.me/dashforge/1.1/assets/img/img1.png" />
             <>Злобина О.</>
