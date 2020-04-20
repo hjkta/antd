808169db6c684c2ab6f8f0a9937b6367ea5cc6c6
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-20 21:34:05 +0300

    единый импорт

---
+++ src/pages/Dashboard/data.tsx
@@ -0,0 +1,23 @@
+import { data as card_approved } from "./card_approved";
+import { data as card_created } from "./card_created";
+import { data as card_full } from "./card_full";
+import { data as card_quickyes } from "./card_quickyes";
+import { data as card_signed } from "./card_signed";
+import { data as cash_approved } from "./cash_approved";
+import { data as cash_created } from "./cash_created";
+import { data as cash_full } from "./cash_full";
+import { data as cash_quickyes } from "./cash_quickyes";
+import { data as cash_signed } from "./cash_signed";
+
+export {
+  card_approved,
+  card_created,
+  card_full,
+  card_quickyes,
+  card_signed,
+  cash_approved,
+  cash_created,
+  cash_full,
+  cash_quickyes,
+  cash_signed,
+};
