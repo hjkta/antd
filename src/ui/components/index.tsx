1d183443f87931c757844b10e8a58f5469a47995
pavke <plaha@yandex.ru>
2020-04-21 14:54:33 +0300

    add DatePicker to components import

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import PageHeader from "./PageHeader";
 import Header from "./Header";
+import DatePicker from "./DatePicker";
 
-export { Card, PageHeader, Header };
+export { Card, PageHeader, Header, DatePicker };
