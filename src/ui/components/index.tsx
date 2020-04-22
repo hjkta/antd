1bf2a582f0d639b87705d784424177ad8bdc91ab
Aleksandr Fuks <flaw.ru@gmail.com>
2020-04-22 18:29:33 +0300

    экспорт компонентов

--- src/ui/components/index.tsx
+++ src/ui/components/index.tsx
@@ -1,5 +1,7 @@
 import Card from "./Card";
 import PageHeader from "./PageHeader";
 import Header from "./Header";
+import Row from "./Row";
+import Col from "./Col";
 
-export { Card, PageHeader, Header };
+export { Card, PageHeader, Header, Row, Col };
